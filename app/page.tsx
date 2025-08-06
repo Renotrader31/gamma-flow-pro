5c143833c5befb33e7b11fc058543d89d60037db
Renotrader31 <lagiog31@gmail.com>
2025-08-06 08:16:44 -0700

    Fix React import issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { 
   Search, Filter, TrendingUp, TrendingDown, 
   Activity, BarChart3, Zap, Clock,
